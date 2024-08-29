37abe71f522fcce5acc7c058f405b9bac128d4cc
Ganesh <ganesh.oficial158@gmail.com>
2024-08-29 22:32:12 +0530

    port issue resolved

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import {loggingMiddleware} from "./loggingMiddleware.js";
-const port =process.env.PORT || 3000;
-const host = process.env.HOST || "localhost";
+const port =3000;
+// const host = process.env.HOST || "localhost";
 const app = express();
 app.use(express.json());
 app.use(loggingMiddleware);
@@ -12,6 +12,6 @@ app.get("/", (req, res) => {
 });
 
 
-app.listen(port,host, () => {
+app.listen(port, () => {
     console.log("Server is running on port ", port);
 });
